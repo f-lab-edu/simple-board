9e67bf1337143693c2049fb7d7babdc3cdf1d51b
yeonwlee <tlstprl1806@naver.com>
2024-03-03 00:15:11 +0900

    Chore: 오타 수정

--- services/login.py
+++ services/login.py
@@ -7,13 +7,13 @@ from schemas.login import LoginData, User, Token
 
 user_db: dict[str: User] = {"firstUser": {"id":"firstUser", "user_name":"뉴비", "password":"1234"}}
 
-def authenicate_user(login_data: LoginData) -> User:
+def authenticate_user(login_data: LoginData) -> User:
     if login_data.id in user_db and user_db[login_data.id]["password"] == login_data.password:
         return user_db[login_data.id]
     return None
 
 def generate_token(login_data: LoginData) -> Token:
-    user_data: dict = authenicate_user(login_data)
+    user_data: dict = authenticate_user(login_data)
     if not user_data:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid User")
     expire_time: datetime = datetime.utcnow() + timedelta(minutes=TokenConfig.TOKEN_EXPIRE_MINUTES)
