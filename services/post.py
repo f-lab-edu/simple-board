78e93f93c0f3d33483fa8cd1c3c37dc7876f8aa9
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-05 21:37:25 +0900

    타입 힌팅 오류 수정
    Co-authored-by: ydarh <110903858+f-lab-ydrah@users.noreply.github.com>

--- services/post.py
+++ services/post.py
@@ -3,7 +3,7 @@ from typing import List
 
 from schemas.post import Post
 
-POST_DB: dict[str: Post] = {"test1": Post(id="test1", title="안녕하세요~", content="반갑습니다", author="admin", created_at=datetime.now())}
+POST_DB: dict[str, Post] = {"test1": Post(id="test1", title="안녕하세요~", content="반갑습니다", author="admin", created_at=datetime.now())}
 
 def get_post_list() -> List[Post]:
     return POST_DB.values()
