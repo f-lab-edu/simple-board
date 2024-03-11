222f3e093416c0d8c2a9b7171dcf2dbda74fddad
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-11 16:30:14 +0900

    Refactor: 딕셔너리.values() 를 리스트로 변환해 리턴
    Co-authored-by: ydarh <110903858+f-lab-ydrah@users.noreply.github.com>

--- services/post.py
+++ services/post.py
@@ -7,7 +7,7 @@ from schemas.post import Post, PostCreateInput
 POST_DB: dict[str: Post] = {"1": Post(id="1", title="안녕하세요~", content="반갑습니다", author="admin", created_at=datetime.now())}
 
 def get_post_list() -> List[Post]:
-    return POST_DB.values()
+    return list(POST_DB.values())
 
 def get_post_detail(post_id: str) -> Post:
     return POST_DB[post_id]
