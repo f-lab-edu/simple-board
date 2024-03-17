06ecf3993bcfc63c28f70c22cab4419d07831aad
yeonwlee <tlstprl1806@naver.com>
2024-03-17 20:07:13 +0900

    Chore: 디렉토리명 변경 및 pre-commit 설정

--- simple_board/routers/post.py
+++ simple_board/routers/post.py
@@ -1,11 +1,9 @@
-
-from fastapi import APIRouter, Body, status, HTTPException
-
-from schemas.post import Post, PostCreateInput
 import services.post as post_service
-
+from fastapi import APIRouter, Body, HTTPException, status
+from schemas.post import Post, PostCreateInput
 
 router = APIRouter()
+
 
 @router.get("/", response_model=list[Post], status_code=status.HTTP_200_OK)
 def get_post_list_api() -> list:
@@ -14,13 +12,13 @@ def get_post_list_api() -> list:
 
 @router.get("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
 def get_post_detail_api(post_id: str) -> Post:
-    if cur_post:= post_service.get_post_detail(post_id):
+    if cur_post := post_service.get_post_detail(post_id):
         return cur_post
-    raise HTTPException(status_code=404,detail="Not Found")
+    raise HTTPException(status_code=404, detail="Not Found")
 
 
 @router.post("/posts/", response_model=dict, status_code=status.HTTP_201_CREATED)
 def insert_post_api(post: PostCreateInput) -> dict:
     if post_service.insert_post(post):
         return {"message": "게시글을 작성했습니다."}
-    raise HTTPException(status_code=400,detail="Bad Request")
+    raise HTTPException(status_code=400, detail="Bad Request")
--- simple_board/schemas/post.py
+++ simple_board/schemas/post.py
@@ -1,6 +1,8 @@
-from datetime import datetime
-from pydantic import BaseModel, Field
 import uuid
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
 
 class Post(BaseModel):
     id: str
@@ -9,11 +11,10 @@ class Post(BaseModel):
     author: str
     created_at: datetime
 
+
 class PostCreateInput(Post):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     title: str
     content: str
     author: str
     created_at: datetime = Field(default_factory=datetime.now)
-    
-
